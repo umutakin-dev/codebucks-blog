06508cb77e1da11bbfbd3b98d57cd54869e1bee0
UMUT AKIN <umutakin@fastmail.com>
2024-10-29 04:41:13 +0300

    chapter 12 - blog page - render mdx

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -1,4 +1,5 @@
 import BlogDetails from "@/components/Blog/BlogDetails";
+import RenderMdx from "@/components/Blog/RenderMdx";
 import Tag from "@/components/Elements/Tag";
 import { allBlogs } from "contentlayer/generated";
 import Image from "next/image";
@@ -37,7 +38,7 @@ export default function BlogPage({ params }) {
 
       <div className="grid grid-cols-12 gap-16 mt-8 px-10">
         <div className="col-span-4">ToC</div>
-        <div className="col-span-8">ToC</div>
+        <RenderMdx blog={blog} />
       </div>
     </article>
   );
