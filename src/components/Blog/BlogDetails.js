08855bcb03858c3707632dde02a74477221f15fb
UMUT AKIN <umutakin@fastmail.com>
2024-11-03 19:26:53 +0300

    chapter 18 - store blog views using supabase

--- src/components/Blog/BlogDetails.js
+++ src/components/Blog/BlogDetails.js
@@ -2,6 +2,7 @@ import { format, parseISO } from "date-fns";
 import Link from "next/link";
 import React from "react";
 import { slug } from "github-slugger";
+import ViewCounter from "./ViewCounter";
 
 const BlogDetails = ({ blog, slug: blogSlug }) => {
   return (
@@ -9,7 +10,9 @@ const BlogDetails = ({ blog, slug: blogSlug }) => {
       <time className="m-3">
         {format(parseISO(blog.publishedAt), "LLLL d, yyyy")}
       </time>
-      <span className="m-3">10 views</span>
+      <span className="m-3">
+        <ViewCounter slug={blogSlug} />
+      </span>
       <div className="m-3">{blog.readingTime.text}</div>
       <Link
         href={`/categories/${slug(blog.tags[0])}`}
