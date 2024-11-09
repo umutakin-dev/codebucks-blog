9d6ac29e90c6bb7fd7f747bb4bb7da26a6246343
UMUT AKIN <umutakin@fastmail.com>
2024-11-10 00:34:40 +0300

    chapter 21 - making it responsive - 01 - logo

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -4,15 +4,28 @@ import profileImage from "@/public/profile-img.png";
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex items-center text-dark">
-      <div className="w-16 rounded-full overflow-hidden border border-solid border-dark mr-4">
+    <Link
+      href="/"
+      className="flex items-center text-dark dark:text-light">
+      <div
+        className="
+          w-12 md:w-16 
+          rounded-full 
+          overflow-hidden 
+          border border-solid border-dark dark:border-light 
+          mr-2 md:mr-4">
         <Image
           src={profileImage}
           alt="umutakin"
           className="w-full h-auto rounded-full"
         />
       </div>
-      <span className="font-bold text-xl">Umut AKIN</span>
+      <span
+        className="
+          font-bold dark:font-semibold 
+          text-lg md:text-xl">
+        Umut AKIN
+      </span>
     </Link>
   );
 };
