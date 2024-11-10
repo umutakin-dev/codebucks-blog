ed68482cb335a8fbbcd53ec56b80bc1688b00598
UMUT AKIN <umutakin@fastmail.com>
2024-11-10 21:37:26 +0300

    chapter 25 - adding googles structured json-ld

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -62,19 +62,48 @@ export async function generateMetadata({ params }) {
 export default function BlogPage({ params }) {
   const blog = allBlogs.find((blog) => blog._raw.flattenedPath === params.slug);
 
+  let imageList = [siteMetadata.socialBanner];
+  if (blog.image) {
+    imageList = typeof (blog.image.filePath === "string")
+      ? [siteMetadata.siteUrl + blog.image.filePath.replace("../public", "")]
+      : blog.image;
+  }
+
+  const jsonLd = {
+    "@context": "https://schema.org",
+    "@type": "NewsArticle",
+    headline: blog.title,
+    description: blog.description,
+    image: imageList,
+    datePublished: new Date(blog.publishedAt).toISOString(),
+    dateModified: new Date(blog.updatedAt || blog.publishedAt).toISOString(),
+    author: [
+      {
+        "@type": "Person",
+        name: blog?.author ? [blog.author] : siteMetadata.author,
+        url: siteMetadata.linkedin,
+      },
+    ],
+  };
+
   return (
-    <article>
-      <div className="mb-8 text-center relative w-full h-[70vh] bg-dark">
-        <div className="w-full z-10 flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <Tag
-            name={blog.tags[0]}
-            link={`/categories/${slug(blog.tags[0])}`}
-            className="
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <article>
+        <div className="mb-8 text-center relative w-full h-[70vh] bg-dark">
+          <div className="w-full z-10 flex flex-col items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <Tag
+              name={blog.tags[0]}
+              link={`/categories/${slug(blog.tags[0])}`}
+              className="
               px-6 py-2
               text-sm"
-          />
-          <h1
-            className="
+            />
+            <h1
+              className="
               inline-block
               w-5/6 
               mt-6 
@@ -82,42 +111,42 @@ export default function BlogPage({ params }) {
               text-2xl md:text-3xl lg:text-5xl 
               leading-normal 
               relative">
-            {blog.title}
-          </h1>
-        </div>
-        <div
-          className="
+              {blog.title}
+            </h1>
+          </div>
+          <div
+            className="
             absolute top-0 left-0 right-0 bottom-0 
             h-full 
             bg-dark/60 dark:bg-dark/40"
+          />
+          <Image
+            src={blog.image.filePath.replace("../public", "")}
+            placeholder="blur"
+            blurDataURL={blog.image.blurhashDataUrl}
+            alt={blog.title}
+            width={blog.image.width}
+            height={blog.image.height}
+            className="aspect-square w-full h-full object-cover object-center"
+            priority
+            sizes="100vw"
+          />
+        </div>
+        <BlogDetails
+          blog={blog}
+          slug={params.slug}
         />
-        <Image
-          src={blog.image.filePath.replace("../public", "")}
-          placeholder="blur"
-          blurDataURL={blog.image.blurhashDataUrl}
-          alt={blog.title}
-          width={blog.image.width}
-          height={blog.image.height}
-          className="aspect-square w-full h-full object-cover object-center"
-          priority
-          sizes="100vw"
-        />
-      </div>
-      <BlogDetails
-        blog={blog}
-        slug={params.slug}
-      />
 
-      <div
-        className="
+        <div
+          className="
           grid 
           grid-cols-12 
           gap-y-8 lg:gap-8 sxl:gap-16 
           mt-8 
           px-5 md:px-10">
-        <div className="col-span-12 lg:col-span-4">
-          <details
-            className="
+          <div className="col-span-12 lg:col-span-4">
+            <details
+              className="
               max-h-[80vh] 
               p-4 
               sticky top-6 
@@ -126,20 +155,20 @@ export default function BlogPage({ params }) {
               text-dark dark:text-light 
               rounded-lg 
               overflow-hidden overflow-y-auto"
-            open>
-            <summary className="text-lg font-semibold capitalize cursor-pointer">
-              Table of Contents
-            </summary>
-            <ul className="mt-4 font-in text-base">
-              {blog.toc.map((heading) => {
-                return (
-                  <li
-                    key={`#${heading.slug}`}
-                    className="py-1 ">
-                    <a
-                      href={`#${heading.slug}`}
-                      data-level={heading.level}
-                      className="
+              open>
+              <summary className="text-lg font-semibold capitalize cursor-pointer">
+                Table of Contents
+              </summary>
+              <ul className="mt-4 font-in text-base">
+                {blog.toc.map((heading) => {
+                  return (
+                    <li
+                      key={`#${heading.slug}`}
+                      className="py-1 ">
+                      <a
+                        href={`#${heading.slug}`}
+                        data-level={heading.level}
+                        className="
                         data-[level=two]:pl-0 
                         data-[level=two]:pt-2 
                         data-[level=two]:border-t 
@@ -147,21 +176,22 @@ export default function BlogPage({ params }) {
                         border-solid border-dark/40
                         flex 
                         items-center justify-start">
-                      {heading.level === "three" ? (
-                        <span className="flex w-1 h-1 rounded-full bg-dark mr-2">
-                          &nbsp;
-                        </span>
-                      ) : null}
-                      <span className="hover:underline">{heading.text}</span>
-                    </a>
-                  </li>
-                );
-              })}
-            </ul>
-          </details>
+                        {heading.level === "three" ? (
+                          <span className="flex w-1 h-1 rounded-full bg-dark mr-2">
+                            &nbsp;
+                          </span>
+                        ) : null}
+                        <span className="hover:underline">{heading.text}</span>
+                      </a>
+                    </li>
+                  );
+                })}
+              </ul>
+            </details>
+          </div>
+          <RenderMdx blog={blog} />
         </div>
-        <RenderMdx blog={blog} />
-      </div>
-    </article>
+      </article>
+    </>
   );
 }
