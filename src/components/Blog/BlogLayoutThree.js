499fc3d385a7d77cee0cc1e93b17edbf03b3dbc5
UMUT AKIN <umutakin@fastmail.com>
2024-11-10 13:42:26 +0300

    chapter 21 - making it responsive - 05 - recent posts + blog layout three

--- src/components/Blog/BlogLayoutThree.js
+++ src/components/Blog/BlogLayoutThree.js
@@ -5,8 +5,14 @@ import React from "react";
 
 const BlogLayoutThree = ({ blog }) => {
   return (
-    <div className="group flex flex-col items-center text-dark">
-      <Link href={blog.url} className="h-full rounded-xl overflow-hidden">
+    <div
+      className="
+        group 
+        flex flex-col items-center 
+        text-dark dark:text-light">
+      <Link
+        href={blog.url}
+        className="h-full rounded-xl overflow-hidden">
         <Image
           src={blog.image.filePath.replace("../public", "")}
           placeholder="blur"
@@ -14,22 +20,48 @@ const BlogLayoutThree = ({ blog }) => {
           alt={blog.title}
           width={blog.image.width}
           height={blog.image.height}
-          className="aspect-[4/3] w-full h-full object-cover object-center group-hover:scale-105 transition-all ease duration-300"
+          className="
+            aspect-[4/3] 
+            w-full h-full 
+            object-cover object-center 
+            group-hover:scale-105 transition-all ease duration-300"
         />
       </Link>
       <div className="flex flex-col w-full mt-4">
-        <span className="uppercase text-accent font-semibold text-sm">
+        <span
+          className="
+            uppercase 
+            text-accent dark:text-accentDark 
+            font-semibold 
+            text-xs sm:text-sm">
           {blog.tags[0]}
         </span>
-        <Link href={blog.url} className="inline-block my-1">
-          <h2 className="font-semibold capitalize text-lg">
-            <span className="bg-gradient-to-r from-accent/50 to-accent/50 bg-[length:0px_6px] group-hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat transition-[background-size] duration-500">
+        <Link
+          href={blog.url}
+          className="inline-block my-1">
+          <h2
+            className="
+              font-semibold 
+              capitalize 
+              text-base sm:text-lg">
+            <span
+              className="
+                bg-gradient-to-r 
+                from-accent/50 to-accent/50 
+                dark:from-accentDark/50 dark:to-accentDark/50
+                bg-[length:0px_6px] group-hover:bg-[length:100%_6px] bg-left-bottom bg-no-repeat 
+                transition-[background-size] duration-500">
               {blog.title}
             </span>
           </h2>
         </Link>
 
-        <span className="capitalize text-dark/50 font-semibold text-base">
+        <span
+          className="
+            capitalize 
+            text-dark/50 dark:text-light/50 
+            font-semibold 
+            text-sm sm:text-base">
           {format(new Date(blog.publishedAt), "yyyy-MM-dd")}
         </span>
       </div>
